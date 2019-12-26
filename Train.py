cccc6f5691a5fe5f0743856090e30ce58b331298
Eren Balatkan <e.balatkan@gmail.com>
2019-12-26 22:24:23 +0300

    Add files via upload

--- Train.py
+++ Train.py
@@ -3,14 +3,21 @@ import BTS
 import os
 import torch
 
-experiment_name = "Balatkan"
-continue_training = True
+experiment_name = "Balatkan"  # This determines folder names used for saving tensorboard logs and model files
+dataset_path = "e://Code/Tez/bts_eren/kitti"
+
+continue_training = False
 total_epochs = 50
 
+# This repository uses gradient accumilation to support bigger batch sizes then what
+# would fit into memory, effective_batch_size determines total batch size with accumilation.
+# batch size represents running batch size
+# In example of effective batch size 16 and batch size of 2, batches will be accumilated 8 times, resulting in effective
+# batch size of 16
 effective_batch_size = 16
 batch_size = 2
 
-dataloader = KittiDataLoader.KittiDataLoader(batch_size)
+dataloader = KittiDataLoader.KittiDataLoader(batch_size, dataset_path)
 model = BTS.BtsController(experiment_name, backprop_frequency=effective_batch_size/batch_size)
 
 saved_models_path = os.path.join("models", experiment_name)
